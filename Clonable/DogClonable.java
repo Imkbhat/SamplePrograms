707db6d315bc3bba6f3ec686c620963b037ad096
Kiran B S <kbs71190@gmail.com>
2019-06-25 17:04:38 +0530

    Add files via upload

---
+++ Clonable/DogClonable.java
@@ -0,0 +1,26 @@
+public class DogClonable implements Cloneable {
+	
+	private String dogName;
+	public DogClonable (String dogName) {
+		this.dogName = dogName;
+	}
+	public String getDogName() {
+		return dogName;
+	}
+	
+	@Override
+	public Object clone() throws CloneNotSupportedException {
+		return (DogClonable)super.clone();
+	}
+	
+	public static void main(String[] args) {
+		DogClonable dc = new DogClonable("Tommy");
+			try {
+				DogClonable dc2 = (DogClonable) dc.clone();
+				System.out.println(dc2.getDogName());
+			} catch(CloneNotSupportedException ex) {
+				ex.printStackTrace();
+			}
+	}
+
+}
