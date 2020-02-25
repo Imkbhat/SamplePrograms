9179afac070f2fbf22df26551b037f4dc4cb6ee6
Kiran B S <kbs71190@gmail.com>
2020-02-25 19:07:01 +0530

    Create NumOddOrEvan.java

---
+++ NumOddOrEvan.java
@@ -0,0 +1,21 @@
+
+
+public class NumOddOrEvan {
+
+  public static boolean oddOrEven(int num) {
+ 
+    return (num % 2 == 0); //bad idea, statistically it may produce bad result over a period of time.
+    
+    return (num & 1) ! = 0; //Good Idea.
+  
+  }
+  
+  public static void main(String[] args) {
+    
+    if(oddOrEven(5)) {
+      System.out.println("Evan Number");
+    } else {
+      System.out.println("Odd Number");
+    }
+  }
+}
